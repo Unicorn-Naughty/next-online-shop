ccad246df476e37825df9231c99d5a4754de94d1
Max <gccccvbnnnn@mail.ru>
2024-11-10 20:25:32 +0700

    1.0

---
+++ constants/const-prisma/categories.ts
@@ -0,0 +1,95 @@
+export const categories = [
+    {
+        name: "Парфюмерия",
+    },
+    {
+        name: "Очищение"
+    },
+    {
+        name: "Пилинги и скрабы"
+    },
+    {
+        name: "Тоники"
+    },
+    {
+        name: "Кремы"
+    },
+    {
+        name: "Сыворотки"
+    },
+
+]
+export const subcategories = [
+    {
+        name: 'Для мужчин',
+        categoryId: 1,
+    },
+    {
+        name: 'Для женщин',
+        categoryId: 1,
+    },
+    {
+        name: 'Для детей',
+        categoryId: 1,
+    },
+    {
+        name: 'Крафтовое мыло',
+        categoryId: 2,
+    },
+    {
+        name: 'Шампуни',
+        categoryId: 2,
+    },
+    {
+        name: 'Гели для душа',
+        categoryId: 2,
+    },
+    {
+        name: 'Для тела',
+        categoryId: 3,
+    },
+    {
+        name: 'Оживительные',
+        categoryId: 3,
+    },
+    {
+        name: 'Молодильные',
+        categoryId: 4,
+    },
+    {
+        name: 'Тонизирующие',
+        categoryId: 4,
+    },
+    {
+        name: 'Бодрящие',
+        categoryId: 4,
+    },
+    {
+        name: 'Для лица',
+        categoryId: 5,
+    },
+    {
+        name: 'Для тела',
+        categoryId: 5,
+    },
+    {
+        name: 'Для ног',
+        categoryId: 5,
+    },
+    {
+        name: 'Интимная зона',
+        categoryId: 5,
+    },
+    {
+        name: 'На глицирине',
+        categoryId: 6,
+    },
+    {
+        name: 'На молоке',
+        categoryId: 6,
+    },
+    {
+        name: 'На масле',
+        categoryId: 6,
+    },
+]
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,11 +1,67 @@
+import { PromocodeName, UserRole } from "@prisma/client";
 import { prisma } from "./prisma-client"
+import { categories, subcategories } from "@/constants/const-prisma/categories";
 
-async function up() { }
-async function down() { }
+async function up() {
+
+
+    await prisma.product.createMany({})
+    await prisma.category.createMany({
+        data: categories
+    })
+    await prisma.categoryItem.createMany({
+        data: subcategories
+    })
+    await prisma.user.createMany({
+        data: [
+            {
+                number: '89639562472',
+                fullName: 'Максим Максимович',
+                address: 'Пупкина залупкина',
+                sex: 'Мужчина',
+                town: 'Красноярск',
+                email: 'admin@mail.ru',
+                birthday: '23.04.2003',
+                bonus: 125,
+                role: UserRole.ADMIN
+            },
+            {
+                number: '87777777777',
+                fullName: 'Валерия Максимович',
+                address: 'Пупкина ',
+                sex: 'Женщина',
+                town: 'Красноярск',
+                email: 'val@mail.ru',
+                birthday: '25.04.1999',
+                bonus: 1255,
+                role: UserRole.USER
+            },
+        ]
+    })
+    await prisma.promocode.createMany({
+        data: [
+            {
+                sale: 10,
+                promocodeName: PromocodeName.PROMO_123
+            },
+            {
+                sale: 5,
+                promocodeName: PromocodeName.PROMO_124
+            },
+            {
+                sale: 15,
+                promocodeName: PromocodeName.PROMO_125
+            },
+        ]
+    })
+}
+async function down() {
+    // await prisma.$executeRaw`TRUNCATE TABLE "User" RESTART IDENTITY CASCADE`
+}
 async function main() {
     try {
-        await (down)
-        await (up)
+        await down()
+        await up()
     } catch (error) {
         console.log(error);
 
